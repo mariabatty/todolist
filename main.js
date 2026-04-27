4f8c266be9825331d3321cd31866e68a2424e702
mariabatty <mariabatty07@gmail.com>
2026-04-27 15:03:01 -0500

    change main

--- main.js
+++ main.js
@@ -1,29 +1,19 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow() {
+app.whenReady().then(() => {
   const win = new BrowserWindow({
     width: 600,
     height: 700,
+    icon: path.join(__dirname, 'favicon.png'),
     webPreferences: {
       nodeIntegration: true
     }
   });
 
   win.loadFile('index.html');
-}
-
-app.whenReady().then(createWindow);
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
-});
-
-const win = new BrowserWindow({
-  width: 600,
-  height: 700,
-  icon: path.join(__dirname, 'favicon.png'),
-  webPreferences: {
-    nodeIntegration: true
-  }
 });
